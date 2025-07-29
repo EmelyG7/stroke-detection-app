7199febe4ec4e2adfe9c0bb2c04b9a04680b945b
Justin Quijada Hernandez <justin_quijada@hotmail.com>
2025-07-29 17:25:52 -0400

    cambios

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ app.add_middleware(
         "http://127.0.0.1:5174",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "*"
+        "https://stroke.abacus-erp.com"
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH"],
